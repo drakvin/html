373fa4567f23abf87bc1a2ca84e6f3f2320e6a72
Drakvin <v.bagazkovs@gmail.com>
2018-08-04 18:29:09 +0300

    myPhPadmin DB

---
+++ mansvar/testing.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.7.7
+ */
+
+/**
+ * Database `testing`
+ */
+
+/* `testing`.`answers` */
+$answers = array(
+  array('id' => '1','answers' => 'B. Akunins','parent_question' => '1','correct_answer' => '1'),
+  array('id' => '2','answers' => 'A. Puškins','parent_question' => '1','correct_answer' => '0'),
+  array('id' => '3','answers' => 'D. Doncova','parent_question' => '1','correct_answer' => '0'),
+  array('id' => '4','answers' => 'M. Aarons','parent_question' => '1','correct_answer' => '0'),
+  array('id' => '5','answers' => 'Deguns','parent_question' => '2','correct_answer' => '0'),
+  array('id' => '6','answers' => 'Lempis','parent_question' => '2','correct_answer' => '1'),
+  array('id' => '7','answers' => 'Aurora','parent_question' => '2','correct_answer' => '0'),
+  array('id' => '8','answers' => 'Kvests','parent_question' => '2','correct_answer' => '0'),
+  array('id' => '9','answers' => '2 un Epilogs','parent_question' => '3','correct_answer' => '0'),
+  array('id' => '10','answers' => '4 un Epilogs','parent_question' => '3','correct_answer' => '1'),
+  array('id' => '11','answers' => '6 un Epilogs','parent_question' => '3','correct_answer' => '0'),
+  array('id' => '12','answers' => 'Rūdolfs Blaumanis','parent_question' => '4','correct_answer' => '1'),
+  array('id' => '13','answers' => 'Kārlis Baumanis','parent_question' => '4','correct_answer' => '0'),
+  array('id' => '14','answers' => 'Rainis','parent_question' => '4','correct_answer' => '0'),
+  array('id' => '15','answers' => 'Fricis Brīvzemnieks','parent_question' => '4','correct_answer' => '0'),
+  array('id' => '16','answers' => 'Rihards Bērziņš','parent_question' => '5','correct_answer' => '0'),
+  array('id' => '17','answers' => 'Gunārs Cīrulis','parent_question' => '5','correct_answer' => '0'),
+  array('id' => '18','answers' => 'Kārlis Baumanis','parent_question' => '5','correct_answer' => '1'),
+  array('id' => '19','answers' => 'Augusts Deglavs','parent_question' => '5','correct_answer' => '0'),
+  array('id' => '20','answers' => 'Apbūvēta teritorija, vai tas daļa, kurā iebraušana apzīmeta ar 533. ceļa zīmi, bet izbraukšana - ar 534. ceļa zīmi.','parent_question' => '6','correct_answer' => '1'),
+  array('id' => '21','answers' => 'Apbūvēta pilsētas vai ciema teritorija, kurā iebraukšana apzīmēta ar 519. ceļa zīmi, bet izbraukšana - ar 520. ceļa zīmi.','parent_question' => '6','correct_answer' => '0'),
+  array('id' => '22','answers' => 'Visi varianti ir pareizi','parent_question' => '6','correct_answer' => '0'),
+  array('id' => '23','answers' => 'Atbilstoši Valsts standartam nokomplktēta transportlīdzekļa masa kopā ar vadītāju, bet bez pasažieriem un kravas','parent_question' => '7','correct_answer' => '0'),
+  array('id' => '24','answers' => 'Izgatavotāja noteikta transportlīdzekļa maksimālā pieļaujamā masa kopā ar vadītāju, pasažieriem un kravu.','parent_question' => '7','correct_answer' => '0'),
+  array('id' => '25','answers' => 'Transportlīdzekļa masa kopā ar transportlīdzekļa vadītāju, pasažieriem un kravu.','parent_question' => '7','correct_answer' => '1'),
+  array('id' => '26','answers' => 'Pārsniegt transportlīdzekļa izgatavotāja noteikto masimālo braukšanas ātrumu.','parent_question' => '8','correct_answer' => '0'),
+  array('id' => '27','answers' => 'Traucēt ceļu satiksmi, bez vajadzības braucot ar pārāk mazu ātrumu.','parent_question' => '8','correct_answer' => '0'),
+  array('id' => '28','answers' => 'Strauji bremzēt, ja tas nav nepieciešams ceļu satiksmes drošībai.','parent_question' => '8','correct_answer' => '0'),
+  array('id' => '29','answers' => 'Visi varianti ir pareizi','parent_question' => '8','correct_answer' => '1'),
+  array('id' => '30','answers' => 'Izmantojot lokano vai cieto sakabi','parent_question' => '9','correct_answer' => '0'),
+  array('id' => '31','answers' => 'Daļēji novietojot velkamo mehānisko transportlīdzekli uz velkošā transportlīdzekļa uz speciālas atbalstierīces.','parent_question' => '9','correct_answer' => '0'),
+  array('id' => '32','answers' => 'Visi varianti ir pareizi','parent_question' => '9','correct_answer' => '1'),
+  array('id' => '33','answers' => 'Aizliegts','parent_question' => '10','correct_answer' => '1'),
+  array('id' => '34','answers' => 'Atļauts','parent_question' => '10','correct_answer' => '0')
+);
+
+/* `testing`.`questions` */
+$questions = array(
+  array('id' => '1','questions' => 'Kas uzrakstīja detektīvu "Azazeļ" ?','parent_test' => '2'),
+  array('id' => '2','questions' => 'Kādu darbu uzrakstīja D.Fonvizins?','parent_test' => '2'),
+  array('id' => '3','questions' => 'Cik Tomu ir Ļeva Tolstoja romānā "Karš un miers"?','parent_test' => '2'),
+  array('id' => '4','questions' => 'Kas uzrakstīja noveli "Nāves ēnā"?','parent_test' => '2'),
+  array('id' => '5','questions' => 'Kas uzrakstīja Latvijas himnu?','parent_test' => '2'),
+  array('id' => '6','questions' => 'Dzīvojama zona ir:','parent_test' => '1'),
+  array('id' => '7','questions' => 'Faktiskā masa ir :','parent_test' => '1'),
+  array('id' => '8','questions' => 'Transportlīdzekļa vadītājam aizliegts:','parent_test' => '1'),
+  array('id' => '9','questions' => 'Mehāniskos transportlīdzekļus atļauts vilkt:','parent_test' => '1'),
+  array('id' => '10','questions' => 'Braukt pa tramvaja pretējā virziena sliežu ceļa klātni ...','parent_test' => '1')
+);
+
+/* `testing`.`test` */
+$test = array(
+  array('id_test' => '1','test_name' => 'CSN_Test'),
+  array('id_test' => '2','test_name' => 'Literatūra')
+);
